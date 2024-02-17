c78962bb9330ac5b0670616226911fd084ec0e20
Matthew Malueg <57232899+JKDS87@users.noreply.github.com>
2024-02-17 15:33:36 -0600

    Commit Streamlit Project 1 A

---
+++ Streamlit_Project_1_Core_A.py
@@ -0,0 +1,50 @@
+### Streamlit_Project_1_Core_A.py for Wk3 of Advanced Machine Learning
+# Create a simple app to demonstrate the use of Streamlit features
+
+# Imports
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from io import StringIO
+
+# Function for loading data
+# Adding data caching
+@st.cache_data
+def load_data():
+    fpath =  "Data/sales_2023_cleaned.csv"
+    df = pd.read_csv(fpath)
+    return df
+
+# load the data 
+df = load_data()
+
+##################################
+
+# Add title
+st.title("Sales Price Analysis")
+
+# Display an interactive dataframe
+st.header("Product Sales Data")
+st.dataframe(df, width=800)
+
+# Display descriptive statistics button
+st.markdown('#### Descriptive Statistics')
+if st.button('Show Descriptive Statistics'):
+    st.dataframe(df.describe().round(2))
+
+## Display Summary Information button
+# Create a string buffer to capture content and write the info into the buffer
+buffer = StringIO()
+df.info(buf=buffer)
+summary_info = buffer.getvalue()
+st.markdown("#### Summary Info")
+if st.button('Show Summary Info'):
+    st.text(summary_info)
+
+## Display Null Values button
+st.markdown("#### Null Values")
+if st.button('Show Null Values'):
+    nulls =df.isna().sum()
+    st.dataframe(nulls)
